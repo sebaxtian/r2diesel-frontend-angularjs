3df88dfa76833343bc60f7a7e23347f6d3c3a731
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2019-06-26 00:02:38 -0500

    Agrego funcionalidad para Login

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-console.log("Hello World from your main file!");
+angular.module("index", []).controller("IndexController", [
+  "$scope",
+  "$http",
+  "$window",
+  function IndexController($scope, $http, $window) {
+    console.log("IndexController");
+    // $scope.email = "sebaxtian@mail.co";
+    // $scope.password = "123456";
+    $scope.doLogin = false;
+    $scope.disabled = "";
+    $scope.errorLogin = false;
+    $scope.ingresar = function() {
+      console.log("Function Ingresar");
+      var dataLogin = {
+        email: $scope.email,
+        password: $scope.password
+      };
+      console.log("dataLogin: ", dataLogin);
+      if (!$scope.doLogin) {
+        $scope.doLogin = true;
+        $scope.disabled = "disabled";
+        $scope.errorLogin = false;
+        $http({
+          method: "POST",
+          url: "http://r2diesel-backend.test/api/login",
+          data: JSON.stringify(dataLogin)
+        }).then(
+          function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log("response: ", response);
+            if (response.status == 200 && response.data.success) {
+              var usuario = response.data.data;
+              var api_token = usuario.api_token;
+              localStorage.setItem("usuario", JSON.stringify(usuario));
+              localStorage.setItem("api_token", api_token);
+              $window.location.href = 'dash.html';
+            } else {
+              console.log("Autenticacion invalida");
+              $scope.errorLogin = true;
+            }
+            $scope.doLogin = false;
+            $scope.disabled = "";
+          },
+          function errorCallback(error) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            // console.log("error: ", error);
+            console.log("Autenticacion invalida");
+            $scope.doLogin = false;
+            $scope.disabled = "";
+            $scope.errorLogin = true;
+          }
+        );
+      }
+    };
+  }
+]);
